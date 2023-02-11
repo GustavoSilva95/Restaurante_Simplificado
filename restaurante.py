1d91394b2bc8cbce062b84c50cc4f045a7cc3530
Gustavo Silva <gustavo_vinicius_50@hotmail.com>
2023-02-10 21:26:37 -0300

    Add files

---
+++ restaurante.py
@@ -0,0 +1,46 @@
+import streamlit as st
+
+st.set_page_config(page_title="Restaurante & Bar no NEU", page_icon="🍗", layout="centered")
+
+st.title("Restaurante & Bar no NEU 🍗")
+st.markdown('### Comanda')
+
+products = {
+    "Agua": 2,
+    "Agua com gás": 3,
+    "Almoço": 40,
+    "Cerveja": 10,
+    "Cerveja lata": 5,
+    "Coca 2 litros": 15,
+    "Coca 600": 8,
+    "Coca Ks": 5,
+    "Coca lata": 5,
+    "Cone": 4,
+    "Copinho de pinga": 3,
+    "Garaná 2 litros": 8,
+    "Garaná lata": 5,
+    "Pinga dose": 3,
+    "Porção Peixe": 45,
+    "Porção Torresmo": 30,
+    "Pudim": 3,
+    "Suco": 15,
+}
+
+total_amount = 0
+product_list = []
+
+
+for product, price in products.items():
+    quantity = st.number_input(f"{product}:", value=0)
+    total_amount += quantity * price
+    if quantity > 0:
+        product_list.append((product, quantity))
+
+if st.button("Calcular Total"):
+    st.write(f"**O total da conta é: R$ {total_amount:.2f}**")
+    if product_list:
+        st.write("Lista de Produtos Consumidos:")
+        for product, quantity in product_list:
+            st.write(f"- {product}: {quantity}")
+
+    
